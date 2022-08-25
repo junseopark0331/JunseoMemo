31c4b8158474c0718df9069d20b54ab49da356bb
junseopark0331 <junseopark0331@icloud.com>
2022-08-25 23:40:36 +0900

    메모앱 만들기 #9 - 원하는 포멧으로 날짜 출력하기

--- JunseoMemo/MemoListTableViewController.swift
+++ JunseoMemo/MemoListTableViewController.swift
@@ -8,7 +8,15 @@
 import UIKit
 
 class MemoListTableViewController: UITableViewController {
-
+    let formatter: DateFormatter = {
+        let f = DateFormatter()
+        f.dateStyle = .long
+        f.timeStyle = .short
+        f.locale = Locale(identifier: "Ko_kr")
+        return f
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -36,7 +44,7 @@ class MemoListTableViewController: UITableViewController {
         
         let target = Memo.dummyMemoList[indexPath.row]
         cell.textLabel?.text = target.content
-        cell.detailTextLabel?.text = target.insertDate.description
+        cell.detailTextLabel?.text = formatter.string(from: target.insertDate)
         
         return cell
     }
