bb62f0e1f9356d7057d3c776846238da24acc185
junseopark0331 <junseopark0331@icloud.com>
2022-08-29 22:27:22 +0900

    메모앱 만들기 #10 - 새 메모 쓰기 화면

---
+++ JunseoMemo/ComposeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ComposeViewController.swift
+//  JunseoMemo
+//
+//  Created by 박준서 on 2022/08/29.
+//
+
+import UIKit
+
+class ComposeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
