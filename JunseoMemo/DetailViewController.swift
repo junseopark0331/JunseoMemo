20c780574616d4058a8c80fe0118be74f55e3e2f
junseopark0331 <junseopark0331@icloud.com>
2022-09-15 20:05:20 +0900

    메모앱 만들기 #15 - 메모 보기 화면2

--- JunseoMemo/DetailViewController.swift
+++ JunseoMemo/DetailViewController.swift
@@ -12,7 +12,8 @@ class DetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+       
+        
     }
     
 
@@ -26,4 +27,27 @@ class DetailViewController: UIViewController {
     }
     */
 
+}
+
+extension DetailViewController: UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+        //테이블 뷰가 표시할 셀 개수를 물어볼때 호출되는 메서드
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        switch indexPath.row{
+        case 0:
+            let cell = tableView.dequeueReusableCell(withIdentifier: "memoCell", for: indexPath)
+            return cell
+        case 1:
+            let cell = tableView.dequeueReusableCell(withIdentifier: "dateCell", for: indexPath)
+            return cell
+        default:
+            fatalError()
+            //default에러에서 빈 셀을 보내므로 fatal error를 사용해 크러시가 발생하게해 새로운 코드가 필요하다는 것을 확실히 알려줌.
+        }
+    }
+    
+    
 }
