d85ff727c94fd94d51ba9bfb6b03001edb298f8b
junseopark0331 <junseopark0331@icloud.com>
2022-09-21 12:03:32 +0900

    메모앱 만들기 #21. 편집 취소 확인

--- JunseoMemo/ComposeViewController.swift
+++ JunseoMemo/ComposeViewController.swift
@@ -8,9 +8,9 @@
 import UIKit
 
 class ComposeViewController: UIViewController {
-
-    var editTarget: Memo?
     
+    var editTarget: Memo?
+    var originalMemoContent: String?
     
     
     @IBAction func close(_ sender: Any) {
@@ -23,12 +23,12 @@ class ComposeViewController: UIViewController {
     @IBAction func save(_ sender: Any) {
         
         guard let memo = memoTextView.text,
-            memo.count > 0 else{
+              memo.count > 0 else{
             alert(message: "메모를 입력하세요")
             return
         }
-//        let newMemo = Memo(content: memo)
-//        Memo.dummyMemoList.append(newMemo)
+        //        let newMemo = Memo(content: memo)
+        //        Memo.dummyMemoList.append(newMemo)
         if let target = editTarget{
             target.content = memo
             DataManager.shared.saveContext()
@@ -37,7 +37,7 @@ class ComposeViewController: UIViewController {
             DataManager.shared.addNewMemo(memo)
             NotificationCenter.default.post(name: ComposeViewController.newMemoDidInsert, object: nil)
         }
-            
+        
         
         dismiss(animated: true, completion: nil)
     }
@@ -48,15 +48,59 @@ class ComposeViewController: UIViewController {
         if let memo = editTarget{
             navigationItem.title = "메모 편집"
             memoTextView.text = memo.content
+            originalMemoContent = memo.content
         }else {
             navigationItem.title = "새 메모"
             memoTextView.text = ""
         }
+        memoTextView.delegate = self
+    }
+    
+    override func viewWillAppear(_ animated: Bool){
+        super.viewWillAppear(animated)
         
+        navigationController?.presentationController?.delegate = self
+    }
+    override func viewWillDisappear(_ animated: Bool){
+        super.viewWillDisappear(animated)
+        
+        navigationController?.presentationController?.delegate = nil
+    }
+    
+}
+    
+    extension ComposeViewController: UITextViewDelegate{
+        func textViewDidChange(_ textView: UITextView){
+            if let original = originalMemoContent, let edited = textView.text{
+                if #available(iOS 13.0, *){
+                    isModalInPresentation = original != edited
+                }else{
+                }
+            }
+        }
+    }
+    
+extension ComposeViewController: UIAdaptivePresentationControllerDelegate{
+    func presntationControllerDidAttemptToDismiss(_ presentationController: UIPresentationController){
+        let alert = UIAlertController(title: "알림", message: "편집할 내용을 저장할까요??", preferredStyle: .alert)
+        
+        let okAction = UIAlertAction(title: "확인", style: .default){[weak self](action) in
+            self?.save(action)
+        }
+        alert.addAction(okAction)
+        
+        let cancelAction = UIAlertAction(title: "취소", style: .cancel){[weak self](action) in
+            self?.close(action)
+        }
+        alert.addAction(cancelAction)
+        
+        present(alert, animated: true, completion: nil)
     }
 }
-
-extension ComposeViewController{
-    static let newMemoDidInsert = Notification.Name(rawValue: "newMemoDidInsert")
-    static let memoDidChange = Notification.Name(rawValue: "memoDidChange")
+    
+    extension ComposeViewController{
+        static let newMemoDidInsert = Notification.Name(rawValue: "newMemoDidInsert")
+        static let memoDidChange = Notification.Name(rawValue: "memoDidChange")
+        
+        
 }
