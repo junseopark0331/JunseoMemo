d969ce33a51ac628f65b505308526e4546c0a4a9
junseopark0331 <junseopark0331@icloud.com>
2022-09-22 20:06:08 +0900

    메모앱 만들기 #26. iPad 지원

--- JunseoMemo/DetailViewController.swift
+++ JunseoMemo/DetailViewController.swift
@@ -21,12 +21,18 @@ class DetailViewController: UIViewController {
         return f
     }()
     
-    @IBAction func share(_ sender: Any) {
+    @IBAction func share(_ sender: UIBarButtonItem) {
         
         guard let memo = memo?.content else{return}
         
         let vc = UIActivityViewController(activityItems: [memo], applicationActivities: nil)
         present(vc,animated: true,completion: nil)
+        
+        if let pc = vc.popoverPresentationController{
+            pc.barButtonItem = sender
+        }
+        
+        
     }
     
     
